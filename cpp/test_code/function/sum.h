66353608c3ada67b0096818bd22996ef461bf988
likaihua <leekaihua@foxmail.com>
2024-06-28 21:58:36 +0800

    cpp learning and test

---
+++ cpp/test_code/function/sum.h
@@ -0,0 +1,15 @@
+/*================================================================
+*   Copyright (C) 2024 Goldman Ltd. All rights reserved.
+*   
+*   File：sum.h
+*   Author：leekaihua
+*   Date：20240628
+*   Brife：
+*
+
+
+
+*/
+#pragma once
+
+int func(int a, int b);
